9cedfc3b696fe4fbb6efc152cef435e1249b05c8
EugeneHezron <eugeneaduogo@gmail.com>
2018-07-01 22:46:11 +0300

    Final auth, display entries, edit and create entries code. Only UI and database implementation to go.

--- app/src/main/java/com/example/android/journalapp/EditActivity.java
+++ app/src/main/java/com/example/android/journalapp/EditActivity.java
@@ -2,12 +2,50 @@ package com.example.android.journalapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class EditActivity extends AppCompatActivity {
+
+    Button saveButton;
+    TextView bodyEntry;
+    TextView titleEntry;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit);
+
+        saveButton = (Button) findViewById(R.id.btn_save);
+        titleEntry = (EditText) findViewById(R.id.et_title_entry);
+        bodyEntry = (TextView) findViewById(R.id.et_body_entry);
+
+        Bundle intentBundle = getIntent().getExtras();
+        if (intentBundle != null || !intentBundle.isEmpty()){
+            String title = intentBundle.getString("TITLE_KEY");
+            String body = intentBundle.getString("BODY_KEY");
+            int id = intentBundle.getInt("ID_KEY");
+
+            if(title.isEmpty() || title == null){
+                title = "Enter title";
+            }
+            if(body.isEmpty() || body == null){
+                body = "What's on your mind";
+            }
+            titleEntry.setText(title);
+            bodyEntry.setText(body);
+        }
+
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String title = titleEntry.getText().toString();
+                String body = bodyEntry.getText().toString();
+                JournalEntry journalEntry = new JournalEntry(title, body);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/android/journalapp/MainActivity.java
+++ app/src/main/java/com/example/android/journalapp/MainActivity.java
@@ -7,6 +7,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
 import com.firebase.ui.auth.AuthUI;
@@ -20,15 +23,22 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TITLE_KEY = "title";
+    private static final String BODY_KEY = "body";
+    private static final String ID_KEY = "id";
     private RecyclerView recyclerView;
     private JournalAdapter journalAdapter;
+    LinearLayout entryContainer;
+    ArrayList<JournalEntry> dataForRV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ArrayList<JournalEntry> dataForRV= new ArrayList<JournalEntry>();
+        entryContainer = (LinearLayout) findViewById(R.id.entry_layout_container);
+
+        dataForRV= new ArrayList<JournalEntry>();
         dataForRV = getEntries();
 
         recyclerView = (RecyclerView) findViewById(R.id.rv_entries_list);
@@ -38,6 +48,23 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(journalAdapter);
 
+        entryContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openEditActivity(view);
+            }
+        });
+
+    }
+
+    private void openEditActivity(View view) {
+        int id = view.getId();
+        JournalEntry journalEntry = dataForRV.get(id);
+        Intent intent = new Intent(this, EditActivity.class);
+        intent.putExtra(TITLE_KEY, journalEntry.getEntryTitle());
+        intent.putExtra(BODY_KEY, journalEntry.getEntryBody());
+        intent.putExtra(ID_KEY, id);
+        startActivity(intent);
     }
 
     private ArrayList<JournalEntry> getEntries() {
